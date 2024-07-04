2075ecde587e710276181b98e598b202a9886161
luan.mai <35148022+maituongluanvn@users.noreply.github.com>
2024-07-04 22:35:23 +0700

    setup site config

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '447c508773e74f3e913d1b80d0bdc8c4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Luan Mai',
+  domain: 'luanmai.dev',
+  author: 'Luan Mai',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Luan Mai space',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
